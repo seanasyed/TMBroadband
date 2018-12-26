bc3334196feccfd41d87d83a840ec373f5adc558
Sean Syed <seansyed@usc.edu>
2018-12-26 14:53:24 -0700

    php

--- application/_site/polling.js
+++ application/_site/polling.js
@@ -1,28 +1,6 @@
 self.addEventListener('message', function(e) {
 	
-	var file = new File([], "input.txt"); 
-	
 	while (true) {
-		
-		var rawFile = new XMLHttpRequest();
-	    rawFile.open("GET", "input.txt", true);
-	    rawFile.onreadystatechange = function ()
-	    {
-	        if (rawFile.responseText != null) {
-	        	
-	        	postMessage(rawFile.responseText); 
-	        	
-	        	$.ajax({
-	    		   url: 'removefile.php',
-	    		   type: 'POST', 
-	    		   data: {
-	    			   
-	    		   }
-	    		  });
-	        	
-	        }
-	    }
-	    rawFile.send();
 		
 	}
 	
