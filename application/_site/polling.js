ef506c8e0fc698b84d888c9cad9c2748f5734544
Sean Syed <seansyed@usc.edu>
2018-12-27 08:20:00 -0700

    refresh

--- application/_site/polling.js
+++ application/_site/polling.js
@@ -1,14 +1,14 @@
 self.addEventListener('message', function(e) {
 	while (true) {
 		var rawFile = new XMLHttpRequest();
-	    rawFile.open("GET", "https://github.com/seanasyed/TMBroadband/blob/master/application/input.txt", false);
+	    rawFile.open("GET", "https://raw.githubusercontent.com/seanasyed/TMBroadband/master/application/input.txt", false);
 	    rawFile.onreadystatechange = function ()
 	    {
 	        if(rawFile.readyState === 4)
 	        {
 	            if(rawFile.status === 200 || rawFile.status == 0)
 	            {
-	                self.postMessage(rawFile.responseText.match(/<td id="LC1" class="blob-code blob-code-inner js-file-line">(.*)/);
+	                self.postMessage(rawFile.responseText);
 	                
 	            }
 	        }
