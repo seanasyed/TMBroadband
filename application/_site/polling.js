8c7e9573fb9a94147f7e9238581cf219aa1945da
Sean Syed <seansyed@usc.edu>
2018-12-27 07:57:52 -0700

    refresh

--- application/_site/polling.js
+++ application/_site/polling.js
@@ -1,33 +1,19 @@
-var globalID = "";
-
 self.addEventListener('message', function(e) {
-	
-	var rawFile = new XMLHttpRequest();
-	var currID = ""; 
-	var newID = ""; 
-	
 	while (true) {
-		
-		var newID = globalID;  
-	    rawFile.open("GET", "input.txt", false);
+		var rawFile = new XMLHttpRequest();
+	    rawFile.open("GET", "https://raw.githubusercontent.com/seanasyed/TMBroadband/master/application/input.txt", false);
 	    rawFile.onreadystatechange = function ()
 	    {
 	        if(rawFile.readyState === 4)
 	        {
 	            if(rawFile.status === 200 || rawFile.status == 0)
 	            {
-	                newID = rawFile.responseText;
+	                self.postMessage(rawFile.responseText);
 	                
-	                if (currID != newID) {
-	                	currID = newID; 
-	                	self.postMessage(currID); 
-	                }
 	            }
 	        }
 	    }
-	    
-	    console.log(globalID); 
 	    rawFile.send(null);
-    }
+	}
+	
 }); 
-
--- application/polling.js
+++ application/polling.js
@@ -1,6 +1,19 @@
 self.addEventListener('message', function(e) {
 	while (true) {
-		//poll the JSON file to see if the id was updated
+		var rawFile = new XMLHttpRequest();
+	    rawFile.open("GET", "https://raw.githubusercontent.com/seanasyed/TMBroadband/master/application/input.txt", false);
+	    rawFile.onreadystatechange = function ()
+	    {
+	        if(rawFile.readyState === 4)
+	        {
+	            if(rawFile.status === 200 || rawFile.status == 0)
+	            {
+	                self.postMessage(rawFile.responseText);
+	                
+	            }
+	        }
+	    }
+	    rawFile.send(null);
 	}
 	
 }); 
